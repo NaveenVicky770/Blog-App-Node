50a0f1093b72aeda48d2453758f806fb774f642e
Naveen Kumar Veerapalli <naveenvicky770@gmail.com>
2022-03-25 12:51:34 +0530

    View Engines

--- app.js
+++ app.js
@@ -5,30 +5,32 @@ const express = require("express");
 const app = express();
 
 //Register View Engine
-app.set('view engine','ejs')
+app.set("view engine", "ejs");
 
 //listen for requests
-app.listen(4556);
+app.listen(4556, () => {
+  console.log("Server started listening on pot 4556");
+});
 
 app.get("/", (req, res) => {
-  //   res.send("<h1>Hey Naveen</h1>");
-  // res.sendFile("./views/index.html", { root: __dirname });
-  res.render('index')
+  const blogs = [
+    { title: "Yoshi finds eggs", snippet: "Lorem ipsum dolor sit amet consectetur" },
+    { title: "Mario finds stars", snippet: "Lorem ipsum dolor sit amet consectetur" },
+    { title: "How to defeat bowser", snippet: "Lorem ipsum dolor sit amet consectetur" },
+  ];
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
-  //   res.send("<h1>Hey Naveen</h1>");
-  // res.sendFile("./views/about.html", { root: __dirname });
-  res.render('about')
+  res.render("about", { title: "About" });
 });
 
-//redirects
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create a new Blog" });
 });
 
 //404 page
-app.use((req,res)=>{
-    // res.status(404).sendFile('./views/404.html',{root:__dirname})
-    res.status(404).render('404')
-})
+app.use((req, res) => {
+  // res.status(404).sendFile('./views/404.html',{root:__dirname})
+  res.status(404).render("404", { title: "404" });
+});
