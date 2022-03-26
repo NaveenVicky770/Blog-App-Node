83a0ba1fa80a29c75763bdda356f380c2e406247
Naveen Kumar Veerapalli <naveenvicky770@gmail.com>
2022-03-26 21:03:05 +0530

    connected to mongodb atlas using mongoose

--- app.js
+++ app.js
@@ -2,21 +2,32 @@
 const express = require("express");
 //3rd Party Logger MiddleWare
 const morgan = require("morgan");
+//mongoose to make mongodb atlas communication easy and better
+const mongoose = require("mongoose");
 
 //express app
 const app = express();
 
+//connect to mongodb
+const dbURI =
+  "mongodb+srv://vicky770:test321@blog-site-cluster.3jzby.mongodb.net/Blog-Website-Database?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURI)
+  .then((res) => {
+    console.log("Connected to DB");
+    //listen for requests after db connection has been made
+    app.listen(4556);
+  })
+  .catch((err) => console.log(err));
+
 //Register View Engine
 app.set("view engine", "ejs");
 
-//listen for requests
-app.listen(4556);
-
 //Static files MiddleWare by express
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 //logger MiddleWare
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   const blogs = [
