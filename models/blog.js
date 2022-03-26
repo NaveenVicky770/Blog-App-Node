59c74a526c14bf9a3ab088c28b13d011bbf06700
Naveen Kumar Veerapalli <naveenvicky770@gmail.com>
2022-03-26 21:18:12 +0530

    created blog Model anf blog Schema as required by mongoose

---
+++ models/blog.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const blogSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    snippet: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+//name of model is plularised in lowercase and searched in collections name like - Blog ==> blogs
+const Blog = mongoose.model("Blog", blogSchema);
+
+module.exports = Blog;
